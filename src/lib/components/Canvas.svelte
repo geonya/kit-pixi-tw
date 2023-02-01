6c8f4328aa6741fb163540b0b7afba2d897646a5
geonya <geony8410@gmail.com>
2023-02-01 21:12:00 +0900

    kit pixi twcss init

---
+++ src/lib/components/Canvas.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
+	import * as PIXI from 'pixi.js';
+
+	let elemCanvas: HTMLCanvasElement;
+	let canvas: PIXI.Application;
+	onMount(() => {
+		PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+
+		canvas = new PIXI.Application({
+			view: elemCanvas,
+			resizeTo: document.body,
+			backgroundColor: 0x141414
+		});
+		if (canvas === null) return;
+	});
+	onDestroy(() => {
+		if (canvas !== undefined) canvas.stop();
+	});
+</script>
+
+<canvas id="canvas" bind:this={elemCanvas} />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Canvas from '../lib/components/Canvas.svelte';
+</script>
+
+<Canvas />
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.500);
+	}
+</style>
